4f9da96679329daa6ffac46879e7ce0803cedb6e
troystribling <troy.stribling@gmail.com>
2011-05-04 22:19:39 -0400

    logo and nav menu tweaks, vertical centering landing page.

--- public/imaginaryProducts.js
+++ public/imaginaryProducts.js
@@ -5,7 +5,9 @@ function setPageSize() {
         win_width     = $(window).width(),
         nav_height    = $('#navigation-wrapper').height(),
         nav_padding   = $('#navigation-wrapper').css('padding-bottom').slice(0, -2),
-        footer_height = $('#footer-wrapper').height(),
+        footer_height = $('#footer-wrapper').height() - 1,
+        footer_padding = $('#footer-wrapper').css('padding-top').slice(0, -2),
+        footer_margin  = $('#footer-wrapper').css('margin-top').slice(0, -2),
         page_width    = 1000;    
     if (win_width < 800) {
         page_width = 800;
@@ -13,9 +15,7 @@ function setPageSize() {
         page_width = win_width;
     }
     if (doc_height == win_height) {
-        var footer_padding = $('#footer-wrapper').css('padding-top').slice(0, -2),
-            footer_margin  = $('#footer-wrapper').css('margin-top').slice(0, -2),
-            page_height  = doc_height - footer_height - nav_height - nav_padding - footer_padding - footer_margin;
+        var page_height  = doc_height - footer_height - nav_height - nav_padding - footer_padding - footer_margin;
         if (win_width >= 800 && win_width < 1000) {
             page_height += 15;
         }
@@ -24,4 +24,11 @@ function setPageSize() {
     $('#page').width(page_width);
     $('#navigation').width(page_width);
     $('#footer').width(page_width);
+    
+    var gallery = $('#gallery')
+    if (gallery) {
+        var padding_top = 2 * (page_height - 500) / 5;
+        $('#page-wrapper').css('padding-top', padding_top)
+        $('#page-wrapper').height(page_height - padding_top);
+    }
 }
